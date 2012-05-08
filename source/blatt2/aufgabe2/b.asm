f7de12a81f94e75b0d77db7db9ba1a0d863b048c
Frederik Walk <f_walk09@cs.uni-kl.de>
2012-05-09 00:38:34 +0200

    Aufgabe gelesen...

--- source/blatt2/aufgabe2/b.asm
+++ source/blatt2/aufgabe2/b.asm
@@ -1,56 +1,105 @@
+bits 16
+
 org 0x100
 
 main:
-	
-	.schleife:
-		call read
-		or ax, ax
-		jz .ende
+    
+    mov ah, 0x4a
+    mov bx, 0x10
+    int 0x21
+    
+    .eingabe_schleife:
+        ;Lese Zeichen, beende einlsen bei leerer Eingabe
+        call readChars
+        cmp al,0x0d
+        je .e_ende
+        
+        ;Reserviere einen Paragraphen pro Listeneintrag
+        push ax
+        mov ah, 0x48
+        mov bx, 0x01
+        int 0x21
+        
+        ;Erzeugen Listeneintrag
+        mov es, ax
+        xor di, di
+        pop ax
+        stosw
+        mov ax, [prev_ptr]
+        stosw
+        mov [prev_ptr], es
 
-		mov [next_mem], ax
-		mov [next_mem+2], prev_ptr
-		mov prev_ptr, next_mem
-		add next_mem, 4
-		jmp .schleife
+        jmp .eingabe_schleife
 
-	.ende:
-		
+    .e_ende:
 
+    .ausgabe_schleife:
+        ;Ausgeben bis Nullpointer
+        cmp WORD [prev_ptr], 0
+        je .a_ende
 
-write:
-	
+        ;Wert Laden und Ausgeben
+        mov ds, [prev_ptr]
+        xor si,si
+        lodsw
+        call writeChars
+        call endl
 
+        ;Pointer auf vorheriges Element laden
+        push WORD [prev_ptr]
+        lodsw
+        mov [prev_ptr], ax
 
-;Lese Zahlen ein
-read:
-	mov dx, input_buffer
-	mov ah, 0x0a
-	int 0x21
+        ;Speicher freigeben
+        pop es
+        mov ah, 0x49
+        int 0x21
 
-	xor ax, ax
-	xor dx, dx
-	mov bx, 10
-	mov si, input_buffer
+        jmp .ausgabe_schleife
 
-	.schleife:
-		mov dl, [si]
-		cmp dl, 0x0d ;Enter eingegeben
-		je .ende
+    .a_ende:
+        jmp 0
 
-		sub dl, '0'
-		mul bx
-		add ax, dx
-		inc si
-		jmp  .schleife
-		
-		.ende:
-			ret
+readChars:
+    ;String mit 2 Zeichen einlesen und in ax schreiben
+    mov ah, 0x0a
+    mov BYTE [chars], 3
+    mov dx, chars
+    int 0x21
+    
+    mov si, chars+2
+    lodsw
 
+    push ax
+    call endl
+    pop ax
 
+    ret
 
+writeChars:
+    ;String in ax ausgeben
+    push ax
+    xor dx, dx
+    mov dl, al
+    mov ah, 0x02
+    int 0x21
 
-input_buffer: times 6 db 0
+    pop ax
+    xor dx, dx
+    mov dl, ah
+    mov ah, 0x02
+    int 0x21
+
+    ret
+
+endl:
+    mov ah, 0x02
+    mov dl, 0x0d
+    int 0x21
+    mov dl, 0x0a
+    int 0x21
+
+    ret
+
+chars: times 5 db 0
 prev_ptr: dw 0
-next_mem: equ code_end
-code_end:
-
